86f9aaf611795033826a3b7dd53875b167656878
AntoineDorelon <antoinedorelon@gmail.com>
2021-02-06 11:35:11 +0800

    end of section 30

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,26 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='antoine@dorelon.bzh',
+            password='password123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@dorelon.bzh',
+            password='test123',
+            name='Test user name'
+        )
+
+    def test_user_listed(self):
+        '''Test that users are listed on page'''
+        url = reverse('admin:core_user_changelist')
+
+
+
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -13,3 +13,20 @@ class ModelTests(TestCase):
         )
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the user for a new user is normalized"""
+        email = "antoine@EXAMPLE.COM"
+        user = get_user_model().objects.create_user(email, 'test123')
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test with a user with no email creates error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_superuser(self):
+        """Test to create a new super user"""
+        user = get_user_model().objects.create_superuser('test@dorelon.bzh', 'test123')
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
